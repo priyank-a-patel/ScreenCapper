e40c7d1dfd16ecd2149bc24dbf0a3d968e18a2fb
Priyank Patel <priyankpatel1308@gmail.com>
2021-02-13 17:28:25 -0500

    recording screen functionality

--- src/render.js
+++ src/render.js
@@ -1,15 +1,31 @@
 const { desktopCapturer, remote } = require('electron');
 
+//writer
 const { writeFile } = require('fs');
 
 const { dialog, Menu } = remote;
+
+//video holders
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
 
 // Buttons
 const videoElement = document.querySelector('video');
 
 const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+};
+
 
 const stopBtn = document.getElementById('stopBtn');
+stopBtn.onclick = e => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+};
 
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
@@ -55,4 +71,27 @@ async function selectSource(source) {
     videoElement.srcObject = stream;
     videoElement.play();
 
+    // Create the Media Recorder
+    const options = { mimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    // Register Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+
+    // Updates the UI
+
 }
+
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+    console.log('video data available');
+    recordedChunks.push(e.data);
+}
+
+// Saves the video file on stop
+async function handleStop(e) {
+
+    console.log("stopped")
+}
+
